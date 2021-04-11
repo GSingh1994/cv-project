655f5a64c31947b6bddf41fbad77a99dd1f7dc88
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-04-11 18:51:47 -0500

    Experience component can now be added in more elegant way

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Experience from "./Experience";
 import Header from "./Header";
+const uniqid = require("uniqid");
 
 class Form extends Component {
   constructor(props) {
@@ -13,7 +14,7 @@ class Form extends Component {
       employer: "",
       jobTitle: "",
       duties: [],
-      totalExp: 0,
+      totalExp: [],
     };
     this.handleChange = this.handleChange.bind(this);
     this.addExp = this.addExp.bind(this);
@@ -25,24 +26,22 @@ class Form extends Component {
   }
 
   addExp() {
-    this.setState((state) => ({
-      totalExp: (state.totalExp += 1),
+    let exp = () => (
+      <Experience key={uniqid()} handleChange={this.handleChange} />
+    );
+    this.setState((preState) => ({
+      totalExp: [...preState.totalExp, exp()],
     }));
   }
   render() {
     const { totalExp } = this.state;
-    let element = [];
-    for (let i = 0; i < totalExp; i++) {
-      element.push(<Experience key={i} handleChange={this.handleChange} />);
-    }
     return (
       <div className="Form">
         <Header handleChange={this.handleChange} />
-        <h2>Work Experience</h2>
-        <div>{element}</div>
-        <button onClick={this.addExp}>Add</button>
 
-        {/* <Experience handleChange={this.handleChange} /> */}
+        <h2>Work Experience</h2>
+        <div>{totalExp}</div>
+        <button onClick={this.addExp}>Add</button>
       </div>
     );
   }
